a119d3a3224722b1d7d0cb5ebb9a1f28c0bda518
Nik Azbekyan <nikazbekyan@Nik-Azbekyans-MacBook-Pro.local>
2019-11-01 16:49:25 -0700

    pseudocode update

---
+++ js/main.js
@@ -0,0 +1,31 @@
+//========CONSTANTS========//
+const cards = ['A','K','Q','J','10','9','8','7','6','5','4','3','2']
+const cardCount = 52;
+
+
+//========STATE VARIABLES========//
+let player1DeckCount = 26;
+let compDeckCount = 26;
+let playerCard = '';
+let compCard = '';
+//========CACHED ELEMENTS========//
+messageEl.ducument.getElementById()
+        //this is where the elements would be grabbed from the HTML to be used in JS
+
+//========EVENT LISTENERS========//
+        // this will be where I will state the event listener for the player and also at the end of the game, I will state an event listener for the "play again" button.
+
+
+//========FUNCTIONS========//
+compareCards  
+        //if playerCard > compCard, player takes both cards and places them face up next to his/her current playing deck which is faced down.
+        //if playerCard < compCard, comp takes bith cards and they are placed next to it's current playing deck which is faced down.
+warDuel  
+        //if playerCard == compCard,  top 5 cards from each deck will be placed face down on the play field and the 6th card will be faced up to declare winner.
+
+//========DISPLAYED MESSAGES========//
+messageEl // this is where the massage will be displayed of who won the card battle, or           the whole game
+
+
+//========DISPLAY IMAGES========//
+        //if I have some images, I will include them during the game.
